96190ac9187a69f8512bce3c584ce79744302fc8
Ben Clifford <benc@hawaga.org.uk>
2026-01-14 19:38:44 +0000

    Display 'Repeat forever' step description

--- app.py
+++ app.py
@@ -734,3 +734,8 @@ class RepeatForeverStep(BlockStep):
   def progress_end_step(self):
     # continue from the step we were at before
     return self._step_number + 1
+
+  def render(self, mode, ctx, render_step, y, text_colour):
+    text = "Repeat forever"
+    tw = ctx.text_width(text)
+    ctx.move_to(int(-tw/2), y).rgb(*text_colour).text(text)
