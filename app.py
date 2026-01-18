580a03b9514f679f0ea04547819271190fe62d36
Ben Clifford <benc@hawaga.org.uk>
2026-01-18 16:31:57 +0000

    Split LED picker into the LED step creator UI and a colour picker UI, with the LED step creator delegating to the colour picker

--- app.py
+++ app.py
@@ -468,11 +468,57 @@ class InsertPauseStepUI:
     self.app._mode = EDIT_MODE
 
 
+# TODO: the top STEP UI needs to choose both a colour and a set
+# of LEDs. So possibly the colour picker now splits out into its
+# own UI component, that sits along side a "choose LEDs" picker
+# that initially can be "all" or 1,2,3,4,5,6,7,8,9,10,11,12 or a
+# subset picker.
+
+# Initialise to the colour picker
+# On colour picker select, initalise the LED picker
+# On LED picker select, 
+
 class InsertLEDStepUI:
   def __init__(self, app):
     self.app = app
+    self.rgb = (0,0,0)
+    self.ui_delegate = ColourPicker(app, callback=self.handle_colour_chosen)
+
+  def update(self, delta):
+    self.ui_delegate.update(delta)
+ 
+  def draw(self, ctx):
+    self.ui_delegate.draw(ctx) 
+
+  def handle_colour_chosen(self, rgb):
+    self.ui_delegate._cleanup()
+    self.rgb = rgb
+
+    # TODO: move to next state. which until I do LED picking, is
+    # to create a step and finish.
+
+    r = self.rgb[0]
+    g = self.rgb[1]
+    b = self.rgb[2]
+    self.app.sequence.insert(self.app.sequence_pos, LEDStep(r, g, b))
+    self.app.sequence_pos += 1
+
+    assert self.app.sequence_pos >= 0
+    assert self.app.sequence_pos < len(self.app.sequence)
+
+    # TODO: i think this should move into a completed callback on the
+    # main app: the modification of app state is a clue.
+    self.app._mode = EDIT_MODE
+    self.app.ui_delegate = None
+
+
+class ColourPicker:
+
+  def __init__(self, app, callback):
+    self.app = app
     self.chosen_colour = 0
     self.rgb = (0,0,0)
+    self._callback = callback
     eventbus.on(ButtonDownEvent, self._handle_buttondown, self.app)
 
   def update(self, delta):
@@ -496,7 +542,7 @@ class InsertLEDStepUI:
     ctx.arc(0, 0, 60, 0, 2 * math.pi, True)
     ctx.rgb(*self.rgb).fill()
 
-  def _cleanup():
+  def _cleanup(self):
     eventbus.remove(ButtonDownEvent, self._handle_buttondown, self.app)
 
   def _handle_buttondown(self, event):
@@ -505,23 +551,13 @@ class InsertLEDStepUI:
     elif BUTTON_TYPES["DOWN"] in event.button:
       self.chosen_colour = (self.chosen_colour + 1) % 4
     elif BUTTON_TYPES["CONFIRM"] in event.button:
-      assert self.app.sequence_pos >= 0
-      assert self.app.sequence_pos < len(self.app.sequence)
-
-      r = self.rgb[0]
-      g = self.rgb[1]
-      b = self.rgb[2]
-      self.app.sequence.insert(self.app.sequence_pos, LEDStep(r, g, b))
-      self.app.sequence_pos += 1
-
-      assert self.app.sequence_pos >= 0
-      assert self.app.sequence_pos < len(self.app.sequence)
-
       eventbus.remove(ButtonDownEvent, self._handle_buttondown, self.app)
-      self.app._mode = EDIT_MODE
-      self.app.ui_delegate = None
+      self._callback(self.rgb)
+
+      assert self.app.sequence_pos >= 0
+      assert self.app.sequence_pos < len(self.app.sequence)
     else:
-      print("unhandled button event in InsertLEDStepUI - ignoring")
+      print("unhandled button event in ColourPicker - ignoring")
 
 
 class Step:
@@ -558,11 +594,12 @@ class BlockStep(Step): ...
 class LEDStep(Step):
   def __init__(self, r, g, b):
     self.rgb = (r, g, b)
+    self.leds = list(range(0,12)) # all LEDs
 
   def enter_step(self):
 
     colour = self.rgb
-    for n in range(0,12):
+    for n in self.leds:
       tildagonos.leds[n+1] = colour
     tildagonos.leds.write()
 
