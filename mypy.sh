defa29d6b1788aa2fb2bdc4a221a2a4510ea4632
Ben Clifford <benc@hawaga.org.uk>
2026-05-21 13:43:03 +0000

    check untyped defs in mypy, revealing a bunch of recent breakage

--- mypy.sh
+++ mypy.sh
@@ -4,4 +4,4 @@ source ~/src/badge-2024-software/venv-testing/bin/activate
 
 export PYTHONPATH=/home/benc/src/badge-2024-software/sim/fakes/:/home/benc/src/badge-2024-software/modules
 
-mypy tildagon_sequencer/ ../../modules/system/eventbus.py ../fakes/imu.py
+mypy tildagon_sequencer/ ../../modules/system/eventbus.py ../fakes/imu.py --check-untyped-defs
