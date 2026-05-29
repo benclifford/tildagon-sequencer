056353cdc26a652c62cef6570a42bc63e0c03185
Ben Clifford <benc@hawaga.org.uk>
2026-05-29 14:11:41 +0000

    pull out WhenPlay step

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import time
 
 from .steps.base import BlockStep, Step, EndStep, WhenStep
 from .steps.forever import RepeatForeverStep
+from .steps.whenplay import WhenPlayStep
 
 from .const import LIVE_SIZE
 
@@ -688,35 +689,3 @@ class WhenIMUUpright(WhenStep):
     ctx.move_to(-240, y - LIVE_SIZE/2)
     ctx.line_to(240, y - LIVE_SIZE/2)
     ctx.stroke()
-
-
-class WhenPlayStep(WhenStep):
-
-  def __init__(self):
-    self._start = False
-
-  def poll_for_when(self):
-    if self._start:
-      self._start = False
-      return True
-    else:
-      return False
-
-  def progress_step(self):
-    return False
-
-  def render(self, mode, ctx, render_step, y, text_colour):
-    text = f"When play starts"  # : {self._start}"
-    tw = ctx.text_width(text)
-    ctx.move_to(int(-tw/2), y).rgb(*text_colour).text(text)
-    ctx.rgb(255,0,0).begin_path()
-    ctx.move_to(-240, y - LIVE_SIZE/2)
-    ctx.line_to(240, y - LIVE_SIZE/2)
-    ctx.stroke()
-
-  def reset(self):
-    self._start = True
-
-  def progress_end_step(self):
-    # block here.
-    return False
