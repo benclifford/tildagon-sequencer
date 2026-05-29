9e69cfc8e80a5a4f94ad2e49d42e8bbb1c8e64e3
Ben Clifford <benc@hawaga.org.uk>
2026-05-29 14:09:41 +0000

    factor out repeat forever step

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import sys
 import time
 
 from .steps.base import BlockStep, Step, EndStep, WhenStep
+from .steps.forever import RepeatForeverStep
 
 from .const import LIVE_SIZE
 
@@ -719,14 +720,3 @@ class WhenPlayStep(WhenStep):
   def progress_end_step(self):
     # block here.
     return False
-
-
-class RepeatForeverStep(BlockStep):
-  def progress_end_step(self):
-    # continue from the step we were at before
-    return self._step_number + 1
-
-  def render(self, mode, ctx, render_step, y, text_colour):
-    text = "Repeat forever"
-    tw = ctx.text_width(text)
-    ctx.move_to(int(-tw/2), y).rgb(*text_colour).text(text)
